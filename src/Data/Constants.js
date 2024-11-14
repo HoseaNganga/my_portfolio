6a8b02823b7bbe2a2de0de48ef10754507797119
Hosea Nganga <126312695+HoseaNganga@users.noreply.github.com>
2024-11-14 11:15:40 +0300

    Projects & Experience Add

--- src/Data/Constants.js
+++ src/Data/Constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/HoseaNganga",
   resume:
-    "https://drive.google.com/file/d/1srDtQfybh2Fkq-P90VB5ZbsErAfrQQ65/view?usp=sharing",
+    "https://drive.google.com/file/d/1lzgs8_DuuTCseoIN_48J6-GlGp7AmVqx/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/hosea-nganga-58b6912b0/",
   twitter: "https://x.com/Hozee_nganga?t=ywre-LkLo6HwmdO61WQltQ&s=08",
   youtube: "http://www.youtube.com/@javascriptsavy",
