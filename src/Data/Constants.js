60c52a1ccc40bf9a13fba28892b4b62626a85ae4
Hosea Nganga <hoseanganga27@gmail.com>
2025-05-05 18:14:39 +0300

    Update Constants.js

--- src/Data/Constants.js
+++ src/Data/Constants.js
@@ -1,10 +1,10 @@
 export const Bio = {
   name: "Hosea Nganga",
   roles: [
-    "Software Engineer",
-    "Angular Developer",
-    "Next Js Developer",
-    "React Developer",
+    "a Software Engineer",
+    "an Angular Developer",
+    "a Next Js Developer",
+    "a React Developer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
